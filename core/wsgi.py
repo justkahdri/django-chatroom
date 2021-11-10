cb9000b3c6de45066f3ca45251b3e94c96725114
JustKahdri <joaco.rm512@gmail.com>
2021-11-09 23:27:36 -0300

    initial commit

---
+++ core/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for core project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+import dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+# dotenv settings
+dotenv.load_dotenv(
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_wsgi_application()
